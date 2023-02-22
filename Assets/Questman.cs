97d1fc2f60c85db65e03bbfdad062862738ea9aa
Jef Smet <jef.smet@student.kdg.be>
2023-02-22 11:22:29 +0100

    added health

---
+++ Assets/Questman.cs
@@ -0,0 +1,10 @@
+using StarterAssets;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Questman : MonoBehaviour
+{
+    [SerializeField] FirstPersonController controller;
+    [SerializeField] int heatlh;
+}
