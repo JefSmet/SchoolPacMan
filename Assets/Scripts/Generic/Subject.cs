2aad4df7633694ecc7dc7e6f376538340db4c271
Jef Smet <114728069+JefSmet@users.noreply.github.com>
2023-03-08 19:05:31 +0100

    Subject.cs
    changed ArrayList to List<Observer>

--- Assets/Scripts/Generic/Subject.cs
+++ Assets/Scripts/Generic/Subject.cs
@@ -1,13 +1,14 @@
 // adapted from packtpub "GAME_DEVELOPMENT_PATTERNS_WITH_UNITY_2021"
 
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace QuestMan.Observer
 {
     public abstract class Subject : MonoBehaviour
     {
-        private readonly ArrayList _observers = new ArrayList();
+        private readonly List<Observer> _observers = new List<Observer>();
         public void Attach(Observer observer)
         {
             _observers.Add(observer);
