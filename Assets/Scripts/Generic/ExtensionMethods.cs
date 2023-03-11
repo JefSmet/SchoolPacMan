e18d808af573f348db8a527ed0ec3dd5ff5ce66c
Jef Smet <jef.smet@student.kdg.be>
2023-03-11 15:57:15 +0100

    pot meter defines the speed of player

---
+++ Assets/Scripts/Generic/ExtensionMethods.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class ExtensionMethods
+{
+
+    public static float Remap(this float value, float inputFrom, float inputTo, float outputFrom, float outputTo)
+    {
+        return (value - inputFrom) / (inputTo - inputFrom) * (outputTo - outputFrom) + outputFrom;
+    }
+
+}
